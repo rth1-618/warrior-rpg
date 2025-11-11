129cab7d82c6849805b62d1302d44ce794464d19
rth1-618 <89399687+rth1-618@users.noreply.github.com>
2025-11-11 13:49:24 +0000

    Animation Movement Transition

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,17 +7,28 @@ public class Player : MonoBehaviour
     private float yInput;
     private Rigidbody2D rb;
 
+    private Animator anim;
+
     [SerializeField] private float moveSpeed = 3.5f;
     [SerializeField] private float jumpForce = 8f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
+
     private void Update()
     {
         HandleInput();
         HandleMovement();
+        HandleAnimation();
+    }
+
+    private void HandleAnimation()
+    {
+        bool isMoving = rb.linearVelocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
     }
 
     private void HandleInput()
